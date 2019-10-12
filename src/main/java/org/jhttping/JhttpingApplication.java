04c85d68b085bfd20e1627ffb3b8b707ad16922a
Alexei <achmelev2@Alexeis-Mini.fritz.box>
2019-10-12 17:07:56 +0200

    Init state

---
+++ src/main/java/org/jhttping/JhttpingApplication.java
@@ -0,0 +1,13 @@
+package org.jhttping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhttpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhttpingApplication.class, args);
+	}
+
+}
