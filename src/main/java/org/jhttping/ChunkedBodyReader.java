306200f48f3615f93541cdd20c8ba09ae749602f
Alexei <achmelev2@Alexeis-Mini.fritz.box>
2019-10-20 19:10:17 +0200

    chunked body reader first steps

---
+++ src/main/java/org/jhttping/ChunkedBodyReader.java
@@ -0,0 +1,19 @@
+package org.jhttping;
+
+import java.io.InputStream;
+
+public class ChunkedBodyReader {
+	
+	private byte [] buf;
+	private InputStream source;
+	
+	public ChunkedBodyReader(InputStream source, int bufSize) {
+		buf = new byte[bufSize];
+		this.source = source;
+	}
+	
+	public int read() {
+		return 0;
+	}
+
+}
